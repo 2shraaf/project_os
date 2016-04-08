9d3484a9c781949ac974d7e01473c54c0d45006d
2shraaf <ahmed.awwad.me@gmail.com>
2016-04-08 14:22:32 +0200

    add exc for shell

--- M1/compile.sh
+++ M1/compile.sh
@@ -4,8 +4,13 @@ ld86 -o kernel -d kernel.o kernel_asm.o
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
 dd if=map.img of=floppya.img bs=512 count=1 seek=1 conv=notrunc
 dd if=dir.img of=floppya.img bs=512 count=1 seek=2 conv=notrunc
-gcc -o loadFile loadFile.c
+bcc -ansi -c -o shell.o shell.c
+as86 lib.asm -o lib.o
+ld86 -o shell -d shell.o lib.o
 ./loadFile message.txt
-
+./loadFile tstprg
+./loadFile tstpr2
+./loadFile shell
+bochs -f config.bxrc
 #type ./compile.sh to run
 #may be modified later
